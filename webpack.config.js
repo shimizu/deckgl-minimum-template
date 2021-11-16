7714dd17e698ba77b7b87dc25ff83dcccd4c26d3
shimizu <xxshimizuxx@gmail.com>
2021-11-16 11:02:45 +0900

    fix: cssファイル内でurlを利用可能に

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,13 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: { url: false }
+                    }
+                ],
             }
         ],
     },
