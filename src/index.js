412183d2056db2b2def88d3d5b058a774ba8dd50
shimizu masayuki <xxshimizuxx@gmail.com>
2022-12-16 15:28:21 +0900

    chore: update libs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /// app.js
 import React from 'react';
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import Map from "./Map.js";
 
@@ -12,4 +12,10 @@ function App() {
     );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+
